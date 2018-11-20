bda800fc783a35f494092adfab93a2d567604370
Alfred Blokland <alfred.blokland@hendrikx-itc.nl>
2018-11-20 11:31:40 +0100

    Support multiple targets per source

--- src/main.rs
+++ src/main.rs
@@ -21,16 +21,21 @@ use inotify::{
 use regex::Regex;
 
 #[derive(Debug, Deserialize)]
-struct DataPath {
+struct DataTarget {
     #[serde(with = "serde_regex")]
     regex: Regex,
-    source_directory: String,
-    target_directory: String
+    directory: String
+}
+
+#[derive(Debug, Deserialize)]
+struct DataSource {
+    directory: String,
+    targets: Vec<DataTarget>
 }
 
 #[derive(Debug, Deserialize)]
 struct Settings {
-    paths: Vec<DataPath>
+    sources: Vec<DataSource>
 }
 
 fn main() {
@@ -45,20 +50,20 @@ fn main() {
     let mut inotify = Inotify::init()
         .expect("Failed to initialize inotify");
 
-    let mut watch_mapping = HashMap::new();
+    let mut watch_mapping: HashMap<inotify::WatchDescriptor, DataSource> = HashMap::new();
 
-    for path in s.paths {
-        let source_directory_str = path.source_directory.clone();
+    for data_source in s.sources {
+        let source_directory_str = data_source.directory.clone();
         let source_directory = Path::new(&source_directory_str);
 
         let watch = inotify
             .add_watch(
                 source_directory,
-                WatchMask::CLOSE_WRITE
+                WatchMask::CLOSE_WRITE | WatchMask::MOVED_TO
             )
             .expect("Failed to add inotify watch");
 
-        watch_mapping.insert(watch, path);
+        watch_mapping.insert(watch, data_source);
     }
 
     let mut buffer = [0u8; 4096];
@@ -69,24 +74,24 @@ fn main() {
             .expect("Failed to read inotify events");
 
         for event in events {
-            if event.mask.contains(EventMask::CLOSE_WRITE) {
-                println!("File created: {:?}", event.name.expect("could not decode event name"));
+            if event.mask.contains(EventMask::CLOSE_WRITE) | event.mask.contains(EventMask::MOVED_TO) {
+                println!("File detected: {:?}", event.name.expect("could not decode event name"));
 
                 let name = event.name.expect("Could not decode name");
 
-                let data_path = watch_mapping.get(&event.wd).unwrap();
+                let data_source = watch_mapping.get(&event.wd).unwrap();
 
-                if data_path.regex.is_match(name.to_str().unwrap()) {
-                    let source_path = Path::new(&data_path.source_directory).join(name);
-                    let target_path = Path::new(&data_path.target_directory).join(name);
+                for data_target in &data_source.targets {
+                    if data_target.regex.is_match(name.to_str().unwrap()) {
+                        let source_path = Path::new(&data_source.directory).join(name);
+                        let target_path = Path::new(&data_target.directory).join(name);
 
-                    println!("Move to: {:?}", target_path);
+                        let rename_result = fs::rename(&source_path, &target_path);
 
-                    let rename_result = fs::rename(&source_path, &target_path);
-
-                    match rename_result {
-                        Err(e) => println!("Error moving {:?} -> {:?}: {:?}", source_path, target_path, e),
-                        Ok(_o) => println!("Successfully moved {:?} -> {:?}", source_path, target_path)
+                        match rename_result {
+                            Err(e) => println!("Error moving {:?} -> {:?}: {:?}", source_path, target_path, e),
+                            Ok(_o) => println!("Moved {:?} -> {:?}", source_path, target_path)
+                        }
                     }
                 }
             }
