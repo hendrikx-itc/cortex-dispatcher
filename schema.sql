604c629c216fc18c1a1ac4e1f3f9360121cf8c7b
Alfred Blokland <alfred.blokland@hendrikx-itc.nl>
2019-04-30 10:59:44 +0200

    Sync versions and use separate database schemas

--- schema.sql
+++ schema.sql
@@ -1,8 +1,16 @@
-create table sftp_download (
+CREATE SCHEMA dispatcher;
+
+CREATE FUNCTION dispatcher.version()
+    RETURNS text
+AS $$
+    SELECT '0.1.2';
+$$ LANGUAGE sql IMMUTABLE;
+
+CREATE TABLE dispatcher.sftp_download (
     id serial,
     created timestamptz not null default now(),
     remote text not null,
     path text not null,
     hash text not null,
-    unique (remote, path)
+    UNIQUE (remote, path)
 );
--- sftp-scanner/schema.sql
+++ sftp-scanner/schema.sql
@@ -1,7 +1,15 @@
-create table sftp_scan (
+CREATE SCHEMA sftp_scanner;
+
+CREATE FUNCTION sftp_scanner.version()
+    RETURNS text
+AS $$
+    SELECT '0.1.2';
+$$ LANGUAGE sql IMMUTABLE;
+
+CREATE TABLE sftp_scanner.scan (
     id serial,
     created timestamptz not null default now(),
     remote text not null,
     path text not null,
-    unique (remote, path)
+    UNIQUE (remote, path)
 );
