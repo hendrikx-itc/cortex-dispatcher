82e127b26c5e700184480d970fe71e24baaac4f7
Alfred Blokland <alfred.blokland@hendrikx-itc.nl>
2019-05-02 15:33:42 +0200

    Refactor SftpDownloaders starting

--- dispatcher/src/cortex.rs
+++ dispatcher/src/cortex.rs
@@ -30,54 +30,50 @@ impl Cortex {
         Cortex { settings }
     }
 
+    fn start_sftp_downloader(sftp_source: settings::SftpSource, db_url: String) -> Addr<SftpDownloader> {
+        SyncArbiter::start(sftp_source.thread_count, move || {
+            let conn = loop {
+                let conn_result = SftpConnection::new(
+                    &sftp_source.address.clone(),
+                    &sftp_source.username.clone(),
+                    sftp_source.compress
+                );
+
+                match conn_result {
+                    Ok(c) => break c,
+                    Err(e) => error!("Could not connect: {}", e)
+                }
+
+                thread::sleep(Duration::from_millis(1000));
+            };
+
+            let db_conn_result = postgres::Connection::connect(db_url.clone(), postgres::TlsMode::None);
+
+            let db_conn = match db_conn_result {
+                Ok(c) => {
+                    info!("Connected to database");
+                    c
+                },
+                Err(e) => {
+                    error!("Error connecting to database: {}", e);
+                    ::std::process::exit(2);
+                }
+            };
+
+            SftpDownloader {
+                config: sftp_source.clone(),
+                sftp_connection: conn,
+                db_connection: db_conn,
+                local_storage_path: String::from("/tmp")
+            }
+        })
+    }
+
     fn start_sftp_downloaders(sftp_sources: Vec<settings::SftpSource>, db_url: String) -> HashMap<String, Addr<SftpDownloader>> {
         sftp_sources
             .iter()
             .map(|sftp_source| {
-                let sftp_source_name = sftp_source.name.clone();
-                let db_url_l = db_url.clone();
-                let owned_sftp_source: settings::SftpSource = sftp_source.clone().clone();
-
-                let sftp_source_settings = sftp_source.clone();
-
-                let addr = SyncArbiter::start(sftp_source_settings.thread_count, move || {
-                    let conn = loop {
-                        let conn_result = SftpConnection::new(
-                            &owned_sftp_source.address.clone(),
-                            &owned_sftp_source.username.clone(),
-                            owned_sftp_source.compress
-                        );
-
-                        match conn_result {
-                            Ok(c) => break c,
-                            Err(e) => error!("Could not connect: {}", e)
-                        }
-
-                        thread::sleep(Duration::from_millis(1000));
-                    };
-
-                    let db_conn_result = postgres::Connection::connect(db_url_l.clone(), postgres::TlsMode::None);
-
-                    let db_conn = match db_conn_result {
-                        Ok(c) => {
-                            info!("Connected to database");
-                            c
-                        },
-                        Err(e) => {
-                            error!("Error connecting to database: {}", e);
-                            ::std::process::exit(2);
-                        }
-                    };
-
-                    SftpDownloader {
-                        config: sftp_source_settings.clone(),
-                        sftp_connection: conn,
-                        db_connection: db_conn,
-                        local_storage_path: String::from("/tmp")
-                    }
-                });
-
-                (sftp_source_name, addr)
+                (sftp_source.name.clone(), Cortex::start_sftp_downloader(sftp_source.clone().clone(), db_url.clone()))
             })
             .collect()
     }
