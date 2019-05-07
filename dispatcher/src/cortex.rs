54d74424ecd479aee7ccc33ed41c06f13c8aa147
Alfred Blokland <alfred.blokland@hendrikx-itc.nl>
2019-05-07 21:05:30 +0200

    Remove use of hash map

--- dispatcher/src/cortex.rs
+++ dispatcher/src/cortex.rs
@@ -19,27 +19,6 @@ use crate::settings;
 use crate::sftp_downloader::{SftpDownloader};
 use crate::metrics_collector::metrics_collector;
 
-fn start_sftp_downloaders(
-    amqp_client: lapin_futures::client::Client<TcpStream>,
-    sftp_sources: Vec<settings::SftpSource>,
-    data_dir: PathBuf,
-    db_url: String,
-) -> HashMap<String, thread::JoinHandle<()>> {
-    sftp_sources
-        .iter()
-        .map(|sftp_source| {
-            (
-                sftp_source.name.clone(),
-                SftpDownloader::start(
-                    amqp_client.clone(),
-                    sftp_source.clone().clone(),
-                    data_dir.clone(),
-                    db_url.clone(),
-                ),
-            )
-        })
-        .collect()
-}
 
 pub fn run(settings: settings::Settings) {
     let local_source_handler_join_handle =
@@ -60,12 +39,14 @@ pub fn run(settings: settings::Settings) {
             error!("Error sending heartbeat: {}", e);
         }));
 
-        start_sftp_downloaders(
-            client,
-            settings.sftp_sources.clone(),
-            settings.storage.directory.clone(),
-            settings.postgresql.url.clone(),
-        );
+        let join_handles: Vec<thread::JoinHandle<()>> = settings.sftp_sources.iter().map(|sftp_source| {
+            SftpDownloader::start(
+                client.clone(),
+                sftp_source.clone(),
+                settings.storage.directory.clone(),
+                settings.postgresql.url.clone()
+            )
+        }).collect();
 
         futures::future::ok(())
     }).map_err(|_| ());
@@ -80,3 +61,4 @@ pub fn run(settings: settings::Settings) {
 
     local_source_handler_join_handle.join().unwrap();
 }
+
