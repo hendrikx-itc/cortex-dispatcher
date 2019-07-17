de9bb124fd6ad5607ba939e35c5018141bd9d177
Alfred Blokland <alfred.blokland@hendrikx-itc.nl>
2019-07-17 20:58:03 +0200

    Log failure to bind to socket

--- sftp-scanner/src/http_server.rs
+++ sftp-scanner/src/http_server.rs
@@ -1,6 +1,6 @@
 use std::thread;
 
-use log::error;
+use log::{error, info};
 
 use actix_rt;
 use actix_web::{middleware, web, App, HttpServer, Responder};
@@ -11,14 +11,22 @@ pub fn start_http_server(addr: std::net::SocketAddr) -> thread::JoinHandle<()> {
     thread::spawn(move || {
         let system = actix_rt::System::new("http_server");
 
-        HttpServer::new(|| {
+        let bind_result = HttpServer::new(|| {
             App::new()
                 .wrap(middleware::Logger::default())
                 .service(web::resource("/metrics").to(metrics))
         })
-        .bind(addr)
-        .unwrap()
-        .start();
+        .bind(addr);
+
+        match bind_result {
+            Ok(http_server) => {
+                info!("Web server bound to address: {}", addr);
+                http_server.start();
+            },
+            Err(e) => {
+                error!("Could not bind to address {}: {}", addr, e);
+            }
+        }
 
         system.run().unwrap();
     })
